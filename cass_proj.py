1a6896d4d0fd6a309f7a3b9e31617f56de6cc542
Daniel Miller <danielmiller5791@gmail.com>
2019-05-27 20:04:39 +0100

    first commit

--- cass_proj.py
+++ cass_proj.py
@@ -34,11 +34,22 @@ def insert_data(session, data):
 	columns = "(song_title, artist_name, year, album_name, single) VALUES (?, ?, ?, ?, ?)"
 	query = session.prepare(core_insert + columns)
 
-	session.execute(
-		session.execute(query, data)
-	)
+	try:
+		session.execute(
+			session.execute(query, data)
+		)
+	except AttributeError as e:
+		print(e)
+		pass
 
+def exec_query(session, query):
 
+		try:
+			rows = session.execute(query)
+			return rows
+		except Exception as e:
+			print(e)
+		
 def main():
 
 	core_keyspace = "CREATE KEYSPACE udac_data_eng_cass	WITH REPLICATION = " 
@@ -49,10 +60,24 @@ def main():
 	create_database(session)
 	session.set_keyspace("udac_data_eng_cass")
 	create_table(session)
-	data1 = ["Let it Booe", "The Beatles", 1970, "Across The Universe", False]
-	# data1 = ["Let it Be", "The Beatles", 1970, "Across The Universe", False]
+
+	data1 = ["Let it Be", "The Beatles", 1970, "Across The Universe", False]
+	data2 = ["Think For Yourself", "The Beatles", 1965, "Rubber Soul", False]
 
 	insert_data(session, data1)
+	insert_data(session, data2)
+
+	all_rows = exec_query(session, 'select * from songs')
+	rows_age = exec_query(session, 'select * from songs where year = 1970 ALLOW FILTERING')
+
+	for r in all_rows:
+		print(r.artist_name, r.album_name, r.year)
+	
+	for r in rows_age:
+		print(r.artist_name, r.album_name, r.year)
+
+	session.shutdown()
+	cluster.shutdown()
 
 
 if __name__ == '__main__':
